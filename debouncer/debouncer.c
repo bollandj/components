1ea03f41f0c9f1380bf120454e0f02cfcf202d08
bollandj <joshuabolland21@gmail.com>
2022-11-11 22:06:04 +0000

    Simplify debounce algorithm, add port masking

--- debouncer/debouncer.c
+++ debouncer/debouncer.c
@@ -3,57 +3,50 @@
 
 void debouncer_init(debouncer_t *debouncer)
 {  
-    debouncer->idx = 0;   
+    debouncer->idx = 0; 
+    debouncer->port_mask = 0xFFFFFFFF;
+    debouncer->port_inv_mask = 0x00000000;
+}
+
+void debouncer_set_mask(debouncer_t *debouncer, uint32_t mask)
+{
+    debouncer->port_mask = mask;    
+}
+
+void debouncer_set_inv_mask(debouncer_t *debouncer, uint32_t inv_mask)
+{
+    debouncer->port_inv_mask = inv_mask; 
 }
 
 void debouncer_update(debouncer_t *debouncer, uint32_t port_state_raw)
 {
+    port_state_raw ^= debouncer->port_inv_mask;
+    port_state_raw &= debouncer->port_mask;
     debouncer->port_states[debouncer->idx++] = port_state_raw;
+    
     if (debouncer->idx >= DEBOUNCER_LENGTH) debouncer->idx = 0; 
 }
 
-uint32_t debouncer_get_port_rising(debouncer_t *debouncer)
+uint32_t debouncer_get_port(debouncer_t *debouncer)
 {
     uint32_t r = 0xFFFFFFFF;
-    uint32_t f = 0x00000000;
+    // uint32_t f = 0x00000000;
+
+    /* A button press will only be registered when all DEBOUNCER_LENGTH port states agree that the button is pressed */
     for (int i=0; i<DEBOUNCER_LENGTH; i++)
     {
         r &= debouncer->port_states[i];
-        f |= debouncer->port_states[i];
+        // f |= debouncer->port_states[i];
     }
-
+  
+    debouncer->port_debounced_prev = debouncer->port_debounced;
     debouncer->port_debounced = r;
-    
-    debouncer->r_prev = r;
-    debouncer->f_prev = f;
 
     return debouncer->port_debounced;
 }
 
-uint32_t debouncer_get_port_falling(debouncer_t *debouncer)
-{
-    uint32_t r = 0xFFFFFFFF;
-    uint32_t f = 0x00000000;
-    for (int i=0; i<DEBOUNCER_LENGTH; i++)
-    {
-        r &= debouncer->port_states[i];
-        f |= debouncer->port_states[i];
-    }
-
-    debouncer->port_debounced = f;
-    
-    debouncer->r_prev = r;
-    debouncer->f_prev = f;
-
-    return debouncer->port_debounced;
-}
-
-uint8_t debouncer_get_pin_rising(debouncer_t *debouncer, uint32_t pin_no)
+bool debouncer_get_pin(debouncer_t *debouncer, uint32_t pin_no)
 {  
-    return (debouncer_get_port_rising(debouncer) >> pin_no) & 0x00000001;
+    return (debouncer_get_port(debouncer) >> pin_no) & 0x00000001;
 }
 
-uint8_t debouncer_get_pin_falling(debouncer_t *debouncer, uint32_t pin_no)
-{  
-    return (debouncer_get_port_falling(debouncer) >> pin_no) & 0x00000001;
-}
--- debouncer/debouncer.h
+++ debouncer/debouncer.h
@@ -3,6 +3,7 @@
 #define DEBOUNCER_H_
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #define DEBOUNCER_LENGTH 4
 
@@ -10,17 +11,21 @@ typedef struct debouncer_t_
 {
     uint32_t port_states[DEBOUNCER_LENGTH];
     uint32_t port_debounced;
-    uint8_t idx;
-    uint32_t r_prev;
-    uint32_t f_prev;
+    uint32_t port_debounced_prev;
+
+    uint32_t port_mask; // 0 = ignore bit, 1 = monitor bit
+    uint32_t port_inv_mask; // 0 = active high, 1 = active low
+
+    int idx;  
 } debouncer_t;
 
 void debouncer_init(debouncer_t *debouncer);
+void debouncer_set_mask(debouncer_t *debouncer, uint32_t mask);
+void debouncer_set_inv_mask(debouncer_t *debouncer, uint32_t inv_mask);
+
 void debouncer_update(debouncer_t *debouncer, uint32_t port_state_raw);
 
-uint32_t debouncer_get_port_rising(debouncer_t *debouncer);
-uint32_t debouncer_get_port_falling(debouncer_t *debouncer);
-uint8_t debouncer_get_pin_rising(debouncer_t *debouncer, uint32_t pin_no);
-uint8_t debouncer_get_pin_falling(debouncer_t *debouncer, uint32_t pin_no);
+uint32_t debouncer_get_port_state(debouncer_t *debouncer);
+bool debouncer_get_pin_state(debouncer_t *debouncer, uint32_t pin_no);
 
 #endif /* DEBOUNCER_H_ */
