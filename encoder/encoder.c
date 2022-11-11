c4042f3194770e634fe27938b782a5b1632b041f
bollandj <joshuabolland21@gmail.com>
2022-11-11 21:35:23 +0000

    Remove callbacks from encoder component
    Component should only really be usable in callback or polled mode, not
    both. Chose polled

--- encoder/encoder.c
+++ encoder/encoder.c
@@ -7,15 +7,7 @@ void encoder_init(encoder_t *encoder)
 {
     encoder->count = 0;
     encoder->a_prev = 0;
-    encoder->b_prev = 0;  
-	encoder->cw_cb = NULL;
-	encoder->ccw_cb = NULL;
-}
-
-void encoder_register_cbs(encoder_t *encoder, void (*_cw_cb)(void), void (*_ccw_cb)(void))
-{
-	encoder->cw_cb = _cw_cb;
-	encoder->ccw_cb = _ccw_cb;
+    encoder->b_prev = 0;
 }
 
 void encoder_update(encoder_t *encoder, uint8_t a, uint8_t b)
@@ -24,13 +16,11 @@ void encoder_update(encoder_t *encoder, uint8_t a, uint8_t b)
 	{
 		if  (b != a)
 		{	// CW rotation 
-			encoder->count++;
-			if (encoder->cw_cb) encoder->cw_cb();	
+			encoder->count++;	
 		}
 		else
 		{	// CCW rotation 
-			encoder->count--;
-			if (encoder->ccw_cb) encoder->ccw_cb();		
+			encoder->count--;	
 		}
 	}
 
